146138ff88508c22f94b3c24bf90259d46a2b15e
jwong1MHS <jwong1@ucvts.tec.nj.us>
2019-03-22 20:06:30 -0400

    Added team number input

--- DisableProcesses.ps1
+++ DisableProcesses.ps1
@@ -1,5 +1,9 @@
 echo "Setting up FRC Driver Station..." ""
 
+[int]$number = Read-Host "Input Team Number"
+$newnumber = ("{0:0000}" -f $number).Insert(2,".")
+$ip = "10." + $newnumber + ".5"
+
 echo "---TURNING OFF SUPERFETCH---"
 net.exe stop superfetch
 
@@ -19,7 +23,7 @@ echo "Done" "" "---ENABLING ETHERNET---"
 netsh interface set interface Ethernet enable
 
 echo "Done" "" "---SETTING STATIC IP---"
-netsh interface ip set address Ethernet static 10.12.57.5 255.0.0.0
+netsh interface ip set address Ethernet static $ip 255.0.0.0
 
 echo "Done" "" "---CHANGING POWER PLAN TO HIGH PERFORMANCE---"
 powercfg.exe /s 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c
